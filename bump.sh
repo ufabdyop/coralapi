235734983e999dde2362bc324d1911520b016f09
Ryan Taylor <ryant@nanofab.utah.edu>
2013-10-31 14:34:38 -0600

    add bumping helpers

---
+++ bump.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "bumping version"
+POMVERSION=`ruby bumpPom.rb`
+
+echo "deploy"
+mvn deploy
+
+echo "commit"
+git commit -a -m "Bumping to version $POMVERSION"
+
+echo "tag"
+git tag v$POMVERSION
+
+echo "push"
+git push
+
+echo "push tag"
+git push -u origin v$POMVERSION
+
+echo "bumping version"
+ruby bumpPom.rb
+
---
+++ bumpPom.rb
@@ -0,0 +1,26 @@
+require 'nokogiri'
+require 'pp'
+
+#slurp version
+#
+pom = Nokogiri::XML(open('pom.xml'))
+pomversionnode = pom.xpath("//pom:project/pom:version", {pom: "http://maven.apache.org/POM/4.0.0"})[0]
+pomversion = pomversionnode.text
+$stderr.puts "original version number: " + pomversion
+versionparts = pomversion.split(/\.|\-/)
+if versionparts[3] == 'SNAPSHOT'
+  versionparts[3] = ''
+else
+  versionparts[2] = versionparts[2].to_i + 1
+  versionparts[3] = '-SNAPSHOT'
+end
+
+#new version
+pomversion = versionparts[0..2].join('.') + versionparts[3]
+$stderr.puts "new version number: " + pomversion
+pomversionnode.content = pomversion
+
+$stderr.puts "writing new version #{pomversion} to pom.xml"
+File.open('pom.xml','w') {|f| pom.write_xml_to f}
+
+puts pomversion
